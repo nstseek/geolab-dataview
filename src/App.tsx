6a94f810f9c4f6e59a5697e50927e20c1aef9e7c
Rafael Schneider <rafael.rls70@gmail.com>
2026-03-20 02:41:03 -0300

    feat: Adds new routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import CssBaseline from '@mui/material/CssBaseline'
-import { ThemeProvider } from '@mui/material/styles'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Layout from '@components/layout/Layout'
-import { HeaderProvider } from '@context/HeaderContext'
-import Home from '@pages/Home'
-import News from '@pages/News'
-import Samples from '@pages/Samples'
-import theme from './theme'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Toaster } from "sonner";
+import Layout from "@components/layout/Layout";
+import { HeaderProvider } from "@context/HeaderContext";
+import Home from "@pages/Home";
+import News from "@pages/News";
+import Samples from "@pages/Samples";
+import theme from "./theme";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -16,25 +17,26 @@ const queryClient = new QueryClient({
       refetchOnWindowFocus: false,
     },
   },
-})
+});
 
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
+        <Toaster theme="dark" richColors position="top-center" />
         <HeaderProvider>
           <BrowserRouter>
             <Routes>
               <Route element={<Layout />}>
                 <Route index element={<Home />} />
-                <Route path='samples' element={<Samples />} />
-                <Route path='news' element={<News />} />
+                <Route path="samples" element={<Samples />} />
+                <Route path="news" element={<News />} />
               </Route>
             </Routes>
           </BrowserRouter>
         </HeaderProvider>
       </ThemeProvider>
     </QueryClientProvider>
-  )
+  );
 }
